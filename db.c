d2f6eebd6c5390b7a58a86588efadf3d5d08c058
anton.pus <anton.slem@gmail.com>
2018-12-19 16:22:59 +0300

    Step 2: add skeleton for compiler

--- db.c
+++ db.c
@@ -8,8 +8,48 @@ struct InputBuffer_t {
     size_t buffer_length;
     ssize_t input_length;
 };
-
 typedef struct InputBuffer_t InputBuffer;
+
+enum PrepareStatementResult_t {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+};
+typedef enum PrepareStatementResult_t PrepareStatementResult;
+
+enum StatementType_t {
+    INSERT,
+    SELECT
+};
+typedef enum StatementType_t StatementType;
+
+struct Statement_t {
+    StatementType type;
+};
+typedef struct Statement_t Statement;
+
+PrepareStatementResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
+    if (strcmp(input_buffer->buffer, "insert") == 0) {
+        statement->type = INSERT;
+        return PREPARE_SUCCESS;
+    }
+
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement) {
+    switch(statement->type) {
+        case INSERT:
+            printf("Inserted!\n");
+            break;
+        case SELECT:
+            printf("Selected!\n");
+            break;
+    }
+}
 
 InputBuffer *new_input_buffer() {
     InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
@@ -29,11 +69,33 @@ void read_input(InputBuffer *input_buffer) {
     // Skip tailing newLine
     input_buffer->input_length = bytes_read - 1;
     input_buffer->buffer[bytes_read - 1] = 0;
-    printf("bytes_read:%zd\n", bytes_read);
 
     if (input_buffer->input_length <= 0) {
         printf("Error reading input\n");
         exit(EXIT_FAILURE);
+    }
+}
+
+bool is_meta_command_in_buffer(InputBuffer *input_buffer) {
+    return input_buffer->buffer[0] == '.';
+}
+
+void do_meta_command(InputBuffer *input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    } else {
+        printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    }
+}
+
+void do_sql_command(InputBuffer *input_buffer) {
+    Statement statement;
+    PrepareStatementResult prepare_result = prepare_statement(input_buffer, &statement);
+
+    if (prepare_result == PREPARE_SUCCESS) {
+        execute_statement(&statement);
+    } else if (prepare_result == PREPARE_UNRECOGNIZED_STATEMENT) {
+        printf("Unrecognized statement found:'%s'\n", input_buffer->buffer);
     }
 }
 
@@ -44,10 +106,10 @@ int main(int argc, char *argv[]) {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-          exit(EXIT_SUCCESS);
+        if (is_meta_command_in_buffer(input_buffer)) {
+            do_meta_command(input_buffer);
         } else {
-          printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+            do_sql_command(input_buffer);
         }
     }
 }
